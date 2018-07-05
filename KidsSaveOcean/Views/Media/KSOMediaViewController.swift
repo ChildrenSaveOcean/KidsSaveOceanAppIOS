3bbf84d59edfa2f60440d6b63d2f41cd4ea39be7
renata <renata.faria.gomes@hotmail.com>
2018-07-04 22:06:03 -0300

    pagination autolayout fixed

--- KidsSaveOcean/Views/Media/KSOMediaViewController.swift
+++ KidsSaveOcean/Views/Media/KSOMediaViewController.swift
@@ -10,58 +10,36 @@ import UIKit
 
 
 //Need to modify main storyboard to test it
-class KSOMediaViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class KSOMediaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    //for example, I will use random images
-    private var storedPictures = [#imageLiteral(resourceName: "Turtle"), #imageLiteral(resourceName: "Whale"), #imageLiteral(resourceName: "WhaleTail"), #imageLiteral(resourceName: "Reef"), #imageLiteral(resourceName: "actinia")]
-    
-    @IBOutlet weak var mediaCollectionView : UICollectionView!
-    
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-       //create an array of stored pictures and return the count of it
-        let numberOfStoragePicture = storedPictures.count
-        return numberOfStoragePicture
-        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return storedPictures.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = mediaCollectionView.dequeueReusableCell(withReuseIdentifier: "mediaCell", for: indexPath) as! KSOMediaCollectionViewCell
-        cell.imgMedia.image = storedPictures[indexPath.row]
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = mediaCollectionView.dequeueReusableCell(withIdentifier: "mediaCell", for: indexPath) as! KSOMediaTableViewCell
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let cellSize = (collectionView.frame.size.width / 2) - 2.5 // 2.3 is equal to the half of space between cell 
-        
-        //here need to put numbers to change the size of the cells
-        return CGSize(width:cellSize , height: cellSize)
-    }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        //space between cells (vertical)
-        return 5
-    }
+    //for example, I will use random images
+    private var storedPictures = [#imageLiteral(resourceName: "Turtle"), #imageLiteral(resourceName: "Whale"), #imageLiteral(resourceName: "WhaleTail"), #imageLiteral(resourceName: "Reef"), #imageLiteral(resourceName: "actinia"), #imageLiteral(resourceName: "Whale"), #imageLiteral(resourceName: "WhaleTail"), #imageLiteral(resourceName: "Reef"), #imageLiteral(resourceName: "actinia"), #imageLiteral(resourceName: "Whale"), #imageLiteral(resourceName: "WhaleTail"), #imageLiteral(resourceName: "Reef"), #imageLiteral(resourceName: "actinia")]
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        //space between cells (horizontal)
-        return 0
-    }
-
+    @IBOutlet weak var mediaCollectionView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //registering my cell to use in table view
-        mediaCollectionView.register(UINib(nibName: "KSOMediaCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "mediaCell")
-        
+        mediaCollectionView.register(UINib(nibName: "KSOMediaTableViewCell", bundle: nil), forCellReuseIdentifier: "mediaCell")
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        mediaCollectionView.estimatedRowHeight = 100
+        mediaCollectionView.rowHeight = UITableViewAutomaticDimension
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-
-    
 }
---
+++ KidsSaveOcean/Views/Media/MediaCells/KSOMediaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  KSOMediaTableViewCell.swift
+//  KidsSaveOcean
+//
+//  Created by Renata on 03/07/2018.
+//  Copyright © 2018 KidsSaveOcean. All rights reserved.
+//
+
+import UIKit
+
+class KSOMediaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
