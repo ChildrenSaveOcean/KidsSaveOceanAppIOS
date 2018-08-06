873e1f5c16d4b7733a6583f8b1ab6d78a9b21e50
Oleg Ivaniv <o.ivaniv.lv@gmail.com>
2018-08-06 18:49:39 +0300

    Added Country picker view pod

--- KidsSaveOcean/Views/Resources/CountryContactsViewController.swift
+++ KidsSaveOcean/Views/Resources/CountryContactsViewController.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import CountryPickerView
 
 final class CountryContactsViewController: UIViewController {
     
     @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var countryPickerView: CountryPickerView!
+    
+    private let viewModel = CountryContactsViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationBar()
+        setupCountryPickerView()
     }
     
     func setupNavigationBar() {
@@ -27,11 +32,34 @@ final class CountryContactsViewController: UIViewController {
         navigationItem.leftBarButtonItem = cancelButton
     }
     
+    func setupCountryPickerView() {
+        countryPickerView.delegate = self
+        countryPickerView.showPhoneCodeInView = false
+        
+        countryPickerView.layer.cornerRadius = 5.0
+        countryPickerView.layer.borderColor = UIColor(red: 0.05, green: 0.37, blue: 0.87, alpha: 1).cgColor
+        countryPickerView.layer.borderWidth = 0.5
+        
+        countryPickerView.countryDetailsLabel.text = countryPickerView.selectedCountry.name
+        countryPickerView.countryDetailsLabel.textAlignment = .center
+        countryPickerView.countryDetailsLabel.font = UIFont.systemFont(ofSize: 18)
+        countryPickerView.flagSpacingInView = -35
+        countryPickerView.flagImageView.isHidden = true
+    }
+    
+    // MARK: - Actions
     @objc func dismissView() {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func didTapView(_ sender: Any) {
         countryTextField.endEditing(true)
+    }
+}
+
+// MARK: - CountryPickerViewDelegate
+extension CountryContactsViewController: CountryPickerViewDelegate {
+    func countryPickerView(_ countryPickerView: CountryPickerView, didSelectCountry country: Country) {
+        countryPickerView.countryDetailsLabel.text = country.name
     }
 }
---
+++ KidsSaveOcean/Views/Resources/CountryContactsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CountryContactsViewModel.swift
+//  KidsSaveOcean
+//
+//  Created by Oleg Ivaniv on 8/6/18.
+//  Copyright © 2018 KidsSaveOcean. All rights reserved.
+//
+
+import UIKit
+
+final class CountryContactsViewModel: NSObject {
+    
+}
