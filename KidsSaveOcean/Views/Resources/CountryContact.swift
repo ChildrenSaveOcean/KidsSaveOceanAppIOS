d7b7b1628cfa1b55ecf43683c5e5b23c56672cc9
Oleg Ivaniv <o.ivaniv.lv@gmail.com>
2018-08-13 15:24:35 +0300

    Added possibility to get countries contacts from Firebase

---
+++ KidsSaveOcean/Views/Resources/CountryContact.swift
@@ -0,0 +1,15 @@
+//
+//  CountryContactDetails.swift
+//  KidsSaveOcean
+//
+//  Created by Oleg Ivaniv on 8/9/18.
+//  Copyright © 2018 KidsSaveOcean. All rights reserved.
+//
+
+import UIKit
+
+struct CountryContact {
+    let name: String
+    let code: String
+    let address: String
+}
